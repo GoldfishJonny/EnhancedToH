71ede095c47a5faf452c03aa566d6a4282fd0af1
Tommy Pho <tommytpho@outlook.com>
2024-05-24 12:19:51 -0700

    Modified currency

--- src/main/java/com/team4/Currency.java
+++ src/main/java/com/team4/Currency.java
@@ -3,28 +3,17 @@ package com.team4;
 public class Currency {
     private int coins;
 
-    public Currency(int coins) {
-        this.coins = coins;
+    public Currency() {
+        this.coins = 0;
     }
 
     public int getCoins() {
         return coins;
     }
 
-    public void setCoins(int coins) {
-        this.coins = coins;
-    }
-
     public void addCoins(int amount) {
         coins += amount;
     }
-
-    public boolean removeCoins(int amount) {
-        if (coins >= amount) {
-            coins -= amount;
-            return true;
-        }
-        return false;
-    }
 }
+
 
