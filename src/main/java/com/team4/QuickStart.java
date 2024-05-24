80a50daf58a9900566b120e6a70e499a6f7db0c0
Cloudy <dutran.pgm@gmail.com>
2024-05-23 18:51:29 -0700

    hiding passwork, + figured out how to export and import info

--- src/main/java/com/team4/QuickStart.java
+++ src/main/java/com/team4/QuickStart.java
@@ -1,22 +1,49 @@
 package com.team4;
 
-import com.mongodb.client.MongoClients;
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+
 
 public class QuickStart {
+
     public static void main(String[] args) {
-        // Connection string from MongoDB Atlas or your MongoDB server
-        String connectionString = "mongodb+srv://dutranpgm:(yS+yG3zy9*QmE$@enhancedhanoi.qdk7tyj.mongodb.net/?retryWrites=true&w=majority&appName=EnhancedHanoi";
+        String connectionString = System.getenv("MONGO_DB_URI");
+        if (connectionString == null || connectionString.isEmpty()) {
+            System.err.println("No MongoDB connection string provided!");
+            return;
+        }
+        MongoClient client = MongoClients.create(connectionString);
 
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            MongoDatabase database = mongoClient.getDatabase("LeaderboardDB"); // replace "test" with your database name
-            System.out.println("Connected to MongoDB!");
-            // You can now perform operations on the database
+        MongoDatabase db = client.getDatabase("SampleDB");
+        MongoCollection<Document> col = db.getCollection("SampleC");
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        Document sampleDoc = new Document("_id", "1").append("name", "John Smith");
+        Document userDoc = new Document("username", "playerOne")
+                .append("highscore1", 5000)
+                .append("highscore2", 3200);
+
+        try {
+            col.insertOne(sampleDoc);
+        } catch (MongoWriteException e) {
+            if (e.getError().getCode() == 11000) {
+                System.out.println("A document with the same _id already exists.");
+            } else {
+                throw e;
+            }
+        }
+
+        readAllDocuments(col);
+        client.close();
+    }
+    private static void readAllDocuments(MongoCollection<Document> collection) {
+        FindIterable<Document> documents = collection.find();
+        for (Document doc : documents) {
+            System.out.println(doc.toJson());
         }
     }
 }
