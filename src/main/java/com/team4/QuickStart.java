703d093ada73e5a2abf5c79ce5f39fea48063700
cloudy <dutran.pgm@gmail.com>
2024-05-23 19:00:07 -0700

    temp fix so that we can all see it work

--- src/main/java/com/team4/QuickStart.java
+++ src/main/java/com/team4/QuickStart.java
@@ -8,16 +8,24 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.FindIterable;
 import org.bson.Document;
 
-
+/**
+ * Class that handles MongoDB stuff
+ * It connects to the MongoDB database that i set up
+ * It imports and exports userdata such as names and highscores
+ * @author Du Tran
+ */
 public class QuickStart {
 
     public static void main(String[] args) {
+        /*
         String connectionString = System.getenv("MONGO_DB_URI");
         if (connectionString == null || connectionString.isEmpty()) {
             System.err.println("No MongoDB connection string provided!");
             return;
         }
-        MongoClient client = MongoClients.create(connectionString);
+         */
+
+        MongoClient client = MongoClients.create("mongodb+srv://dutranpgm:tAZowHn5EzpmXY3c@sample.68tcera.mongodb.net/?retryWrites=true&w=majority&appName=Sample");
 
         MongoDatabase db = client.getDatabase("SampleDB");
         MongoCollection<Document> col = db.getCollection("SampleC");
@@ -46,4 +54,10 @@ public class QuickStart {
             System.out.println(doc.toJson());
         }
     }
+
+    /*
+    I guess the idea now is that when something is added to the data.json file it's also pushed onto the DB
+    I think maybe how this run everything the game ends and it just scans through the entire json file and adds
+    non-duplicates to the DB?
+     */
 }
